31f9b8fcc391f415310f8e3bee08601c3895ccc9
Jonathan Freed <jon.freed@gmail.com>
2025-12-20 13:06:53 +0700

    Collapse ledger/movements breadcrumb to Inventory movements

--- ui/src/components/Breadcrumbs.tsx
+++ ui/src/components/Breadcrumbs.tsx
@@ -2,6 +2,7 @@ import { Link, useLocation } from 'react-router-dom'
 
 function formatLabel(segment: string) {
   if (!segment) return 'Home'
+  if (segment === 'ledger/movements') return 'Inventory movements'
   return segment
     .split('-')
     .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
@@ -14,7 +15,13 @@ function buildPath(segments: string[], index: number) {
 
 export default function Breadcrumbs() {
   const location = useLocation()
-  const segments = location.pathname.split('/').filter(Boolean)
+  const rawSegments = location.pathname.split('/').filter(Boolean)
+  let segments = [...rawSegments]
+
+  if (segments[0] === 'ledger' && segments[1] === 'movements') {
+    const tail = segments.slice(2)
+    segments = ['ledger/movements', ...tail]
+  }
 
   const crumbs = segments.length ? segments : ['home']
 
